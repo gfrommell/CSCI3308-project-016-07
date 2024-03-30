6c682e569be1f7cf91f8f4ec5bd82581bf6f1e7f
Tehya Laughlin <tehyanlaugh.shop@gmail.com>
2024-03-30 15:12:47 -0600

    insert data for all, still need relations

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -1,51 +1,5 @@
 --Relations Tables, Images table
 
-
---DROP TABLE IF EXISTS campgrounds;
-
-CREATE TABLE campgrounds(
-    campground_id VARCHAR(50) PRIMARY KEY,
-    campground_latlong VARCHAR(60),
-    campground_name VARCHAR(60),
-    park_code VARCHAR(4) REFERENCES parks (park_code) ON DELETE CASCADE,
-    campground_url VARCHAR(100)
-);
-CREATE TABLE tours(
-    tour_id VARCHAR(50) PRIMARY KEY,
-    park_code VARCHAR(4) REFERENCES parks (park_code) ON DELETE CASCADE,
-    tour_duration VARCHAR(50),
-    tour_title VARCHAR(50),
-    tour_description VARCHAR(200)
-);
-
-
---DROP TABLE IF EXISTS activities;
-CREATE TABLE activities(
-    activity_id VARCHAR(50) PRIMARY KEY,
-    activity_name VARCHAR(50)
-);
-
---DROP TABLE IF EXISTS things_to_do;
-CREATE TABLE things_to_do(
-    thing_id VARCHAR(50) PRIMARY KEY,
-    thing_description VARCHAR(200),
-    thing_url VARCHAR(100),
-    thing_title VARCHAR(100),
-    thing_duration VARCHAR(30),
-    thing_location VARCHAR(100),
-    park_code VARCHAR(4) REFERENCES parks (park_code) ON DELETE CASCADE
-);
-
-CREATE TABLE events(
-    event_id VARCHAR(50) PRIMARY KEY,
-    event_description VARCHAR(100),
-    park_code VARCHAR(4) REFERENCES parks (park_code) ON DELETE CASCADE,
-    event_title VARCHAR(40),
-    event_start VARCHAR(12),
-    event_location VARCHAR(50)
-);
-
-
 --DROP TABLE IF EXISTS users;
 CREATE TABLE users(
     username VARCHAR(50) PRIMARY KEY,
@@ -63,10 +17,10 @@ CREATE TABLE trips(
 
 --DROP TABLE IF EXISTS days;
 CREATE TABLE days(
-    day_id VARCHAR(50) PRIMARY KEY,
+    day_id SERIAL PRIMARY KEY,
     day_number INTEGER,
     trip_id INTEGER REFERENCES trips (trip_id) ON DELETE CASCADE,
-    campground_id VARCHAR(50) REFERENCES campgrounds (campground_id) ON DELETE CASCADE
+    campground_id text REFERENCES campgrounds (campground_id) ON DELETE CASCADE
 );
 
 --DROP TABLE IF EXISTS images;
@@ -83,98 +37,106 @@ CREATE TABLE images(
 
 --DROP TABLE IF EXISTS parks_to_things;
 CREATE TABLE parks_to_things(
-    park_code VARCHAR(4) REFERENCES parks (park_code) ON DELETE CASCADE,
-    thing_id VARCHAR(50) REFERENCES things_to_do (thing_id) ON DELETE CASCADE
+    park_code text REFERENCES parks (park_code) ON DELETE CASCADE,
+    thing_id text REFERENCES things_to_do (thing_id) ON DELETE CASCADE
 );
 
 --DROP TABLE IF EXISTS things_to_activities;
 CREATE TABLE activities_to_things(
-    thing_id VARCHAR(50) REFERENCES things_to_do (thing_id) ON DELETE CASCADE,
-    activity_id VARCHAR(50) REFERENCES activities (activity_id) ON DELETE CASCADE
+    thing_id text REFERENCES things_to_do (thing_id) ON DELETE CASCADE,
+    activity_id text REFERENCES activities (activity_id) ON DELETE CASCADE
 );
 
 --DROP TABLE IF EXISTS days_to_parks;
 CREATE TABLE days_to_parks(
-    day_id VARCHAR(50) REFERENCES days (day_id) ON DELETE CASCADE, 
-    park_code VARCHAR(4) REFERENCES parks (park_code) ON DELETE CASCADE
+    day_id INTEGER REFERENCES days (day_id) ON DELETE CASCADE, 
+    park_code text REFERENCES parks (park_code) ON DELETE CASCADE
 );
 
 --DROP TABLE IF EXISTS activities_to_parks;
-CREATE TABLE activites_to_parks(
-    activity_id VARCHAR(50) REFERENCES activities (activity_id) ON DELETE CASCADE,
-    park_code VARCHAR(4) REFERENCES parks (park_code) ON DELETE CASCADE
+CREATE TABLE activities_to_parks(
+    activity_id text REFERENCES activities (activity_id) ON DELETE CASCADE,
+    park_code text REFERENCES parks (park_code) ON DELETE CASCADE
 );
 
 --DROP TABLE IF EXISTS images_to_parks;
 CREATE TABLE images_to_parks(
     image_id INTEGER REFERENCES images (image_id) ON DELETE CASCADE,
-    park_code VARCHAR(4) REFERENCES parks (park_code) ON DELETE CASCADE
+    park_code text REFERENCES parks (park_code) ON DELETE CASCADE
 );
 
 --DROP TABLE IF EXISTS campgrounds_to_images;
 CREATE TABLE images_to_campgrounds(
-    campground_id VARCHAR(50) REFERENCES campgrounds (campground_id) ON DELETE CASCADE,
+    campground_id text REFERENCES campgrounds (campground_id) ON DELETE CASCADE,
     image_id INTEGER REFERENCES images (image_id) ON DELETE CASCADE
 );
 
 --DROP TABLE IF EXISTS events_to_images;
 CREATE TABLE images_to_events(
     image_id INTEGER REFERENCES images (image_id) ON DELETE CASCADE,
-    event_id VARCHAR(50) REFERENCES events (event_id) ON DELETE CASCADE
+    event_id text REFERENCES events (event_id) ON DELETE CASCADE
 );
 
 --DROP TABLE IF EXISTS tours_to_images;
 CREATE TABLE images_to_tours(
-    tour_id VARCHAR(50) REFERENCES tours (tour_id) ON DELETE CASCADE,
+    tour_id text REFERENCES tours (tour_id) ON DELETE CASCADE,
     image_id INTEGER REFERENCES images (image_id) ON DELETE CASCADE
 );
 
 --DROP TABLE IF EXISTS tours_to_activities;
 CREATE TABLE activities_to_tours(
-    tour_id VARCHAR(50) REFERENCES tours (tour_id) ON DELETE CASCADE,
-    activity_id VARCHAR(50) REFERENCES activities (activity_id) ON DELETE CASCADE
+    tour_id text REFERENCES tours (tour_id) ON DELETE CASCADE,
+    activity_id text REFERENCES activities (activity_id) ON DELETE CASCADE
 );
 
 --DROP TABLE IF EXISTS days_to_events;
 CREATE TABLE days_to_events(
-    day_id VARCHAR(50) REFERENCES days (day_id) ON DELETE CASCADE,
-     event_id VARCHAR(50) REFERENCES events (event_id) ON DELETE CASCADE
+    day_id INTEGER REFERENCES days (day_id) ON DELETE CASCADE,
+    event_id text REFERENCES events (event_id) ON DELETE CASCADE
 );
 
 --DROP TABLE IF EXISTS days_to_things;
 CREATE TABLE days_to_things(
-    day_id VARCHAR(50) REFERENCES days (day_id) ON DELETE CASCADE,
-    thing_id VARCHAR(50) REFERENCES things_to_do (thing_id) ON DELETE CASCADE
+    day_id INTEGER REFERENCES days (day_id) ON DELETE CASCADE,
+    thing_id text REFERENCES things_to_do (thing_id) ON DELETE CASCADE
 );
 
 --DROP TABLE IF EXISTS days_to_tours;
 CREATE TABLE days_to_tours(
-    day_id VARCHAR(50) REFERENCES days (day_id) ON DELETE CASCADE,
-    tour_id VARCHAR(50) REFERENCES tours (tour_id) ON DELETE CASCADE
+    day_id INTEGER REFERENCES days (day_id) ON DELETE CASCADE,
+    tour_id text REFERENCES tours (tour_id) ON DELETE CASCADE
 );
+
+--DROP TABLE IF EXISTS days_to_tours;
 
 --https://www.postgresql.org/docs/9.3/functions-json.html For doing SQL methods with json data
 
 --INSERT for images under parks, relational images_to_parks, activities_to_parks
 
---images
-INSERT INTO images
-SELECT json_array_elements(images)->> 'caption', json_array_elements(images)->> 'url', json_array_elements(images)->> 'credit', json_array_elements(images)->> 'title', json_array_elements(images)->> 'altText'
-FROM parks;
+    --images
+    INSERT INTO images
+    SELECT json_array_elements(images)->> 'caption', json_array_elements(images)->> 'url', json_array_elements(images)->> 'credit', json_array_elements(images)->> 'title', json_array_elements(images)->> 'altText'
+    FROM parks;
 
---remove duplicate images
-DELETE FROM images 
-WHERE  image_id NOT IN (SELECT Min(image_id) FROM images GROUP BY url);
+    --remove duplicate images
+    DELETE FROM images 
+    WHERE  image_id NOT IN (SELECT Min(image_id) FROM images GROUP BY url);
 
---images_to_parks
-WITH 
-    parkImages AS (
-        SELECT json_array_elements(parks.images)->>'url' AS url, park_code FROM parks 
-    )
-INSERT INTO images_to_parks
-SELECT image_id, parkImages.park_code FROM images INNER JOIN parkImages ON images.url = parkImages.url;
+    --images_to_parks
+    WITH 
+        parkImages AS (
+            SELECT json_array_elements(parks.images)->>'url' AS url, park_code FROM parks 
+        )
+    INSERT INTO images_to_parks
+    SELECT image_id, parkImages.park_code FROM images INNER JOIN parkImages ON images.url = parkImages.url;
 
-
+    --activities_to_parks
+    WITH 
+        parkActivities AS (
+            SELECT json_array_elements(parks.activities)->>'id' AS id, park_code FROM parks 
+        )
+    INSERT INTO activities_to_parks
+    SELECT activity_id, parkActivities.park_code FROM activities INNER JOIN parkActivities ON activities.activity_id = parkActivities.id;
 
 --INSERT for images under tours, relational images_to_tours, activities_to_tours
 
