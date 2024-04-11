89ef14c9b83b7ba72c1f3f8f4e6c570d6351d9b6
Devin Green <degr3336@Colorado.edu>
2024-04-11 14:56:20 -0600

    home endpoint fixed

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -42,36 +42,7 @@ db.connect()
     obj.done(); // success, release the connection;
   })
   .catch(error => {
-    console.log('ERROR:', error.message || error);
-  });
-  extname: 'hbs',
-  layoutsDir: __dirname + '/views/layouts',
-  partialsDir: __dirname + '/views/partials',
-});
-
-// database configuration
-const dbConfig = {
-  host: 'db', // the database server
-  port: 5432, // the database port
-  database: process.env.POSTGRES_DB, // the database name
-  user: process.env.POSTGRES_USER, // the user account to connect with
-  password: process.env.POSTGRES_PASSWORD, // the password of the user account
-};
-
-const db = pgp(dbConfig);
-
-// test your database
-db.connect()
-  .then(obj => {
-    console.log('Database connection successful'); // you can view this message in the docker compose logs
-    obj.done(); // success, release the connection;
-  })
-  .catch(error => {
-    console.log('ERROR:', error.message || error);
-  });
-
-
-// *****************************************************
+    console.log('ERROR:', error.message || error);});
 // *****************************************************
 // <!-- Section 3 : App Settings -->
 // *****************************************************
@@ -106,7 +77,7 @@ const user = {
 
 app.get('/home', (req, res) => {
   if (req.session.user) {
-    
+
     const query = "SELECT trip_title, start_date , number_of_days, trip_progress FROM trips WHERE username = $1;";
 
     db.any(query, [user.username])
@@ -130,67 +101,31 @@ app.get('/', (req, res) => {
 
 app.get('/register', (req, res) => {
   res.render('pages/register');
-});
-
-app.get('/login', (req, res) => {
-app.get('/login', (req, res) => {
+}); app.get('/login', (req, res) => {
   res.render('pages/login');
 });
-
-app.get('/home', (req, res) => {
-  if (req.session.user) {
-
-    const query = "SELECT trip_title, start_date , number_of_days, trip_progress FROM trips WHERE username = $1;";
-
-    db.any(query, [user.username])
-      .then(data => {
-        res.render('pages/home', { data: data });
-      })
-      .catch((err) => {
-        res.render('pages/home', { data: [] });
-        console.log("error")
-      });
-
-
-  } else {
-    res.redirect('/login'); // redirect users to login page  
-  }
-});
-
 // Register
 app.post('/register', async (req, res) => {
   //hash the password using bcrypt library
   const hash = await bcrypt.hash(req.body.password, 10);
-
-
   // To-DO: Insert username and hashed password into the 'users' table
   const username = req.body.username;
   const email = req.body.email;
 
-  if (!username || !hash || !email) {
-    return res.status(400).send('Missing required fields');
-  }
-
   const query = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3);`;
   db.any(query, [username, hash, email])
     .then(data => {
-      res.status(200);
-      res.redirect('/login')
+      res.redirect('/home')
     })
     .catch((err) => {
-      res.status(400);
       res.redirect('/register')
       console.log("error")
     });
 });
 
 app.post('/login', (req, res) => {
-app.post('/login', (req, res) => {
   const username = req.body.username;
   // const password = req.body.password;
-
-  const query = `SELECT username, password, email from users WHERE username = $1;`;
-
 
   const query = `SELECT username, password, email from users WHERE username = $1;`;
 
@@ -249,9 +184,7 @@ app.get('/createTrip', (req, res) => {
   });
 });
 
-app.get('/home', (req, res) => {
-  res.render('pages/home');
-});
+
 
 
 
@@ -265,33 +198,18 @@ app.post("/createTrip", (req, res) => {
   }
   const trip_progress = "Planned"; // this is default
 
-
-  //Insert into trips table and also return trip_id and num_days to be used to insert into days table
-  const queryTrips = `
-    INSERT INTO trips (trip_title, start_date, number_of_days, username, trip_progress)
-    VALUES ($1, $2, $3, $4, $5)
-    RETURNING trip_id, number_of_days; 
+  const query = `
+  INSERT INTO trips (trip_title, start_date, number_of_days, username, trip_progress)
+  VALUES ($1, $2, $3, $4, $5)
   `
 
-  const queryDays = `
-    INSERT INTO days (day_number, trip_id) VALUES ($1, $2);
-  `
-
-  db.task(async task => {
-    // result will have trip_id and number_of_days
-    const result = await task.one(queryTrips, [title, startdate, numDays, username, trip_progress]);
-    for(let i = 1; i <=  numDays; i++){
-
-      await task.none(queryDays, [i, result.trip_id])
-    }
-    
-    
-  })
+  db.any(query, [title, startdate, numDays, username, trip_progress])
     .then(data => {
       res.render('pages/home', {
         message: "Created Trip Successfully!"
       })
     })
+
     .catch(err => {
       res.render('pages/home', {
         error: true,
@@ -301,6 +219,9 @@ app.post("/createTrip", (req, res) => {
     })
 
 });
+
+
+
 
 app.get('/logout', (req, res) => {
   req.session.destroy();
