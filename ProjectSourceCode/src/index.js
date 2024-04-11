38536fcc6d6b03badaf610f2226ff856826c91fa
Derick Sayavong <dsayavong54651@gmail.com>
2024-04-11 14:49:24 -0600

    added date helper

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -14,6 +14,8 @@ const bcrypt = require('bcrypt'); //  To hash passwords
 const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
 const { error } = require('console');
 
+
+
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
 // *****************************************************
@@ -25,6 +27,13 @@ const hbs = handlebars.create({
     partialsDir: __dirname + '/views/partials',
   });
   
+  Handlebars.registerHelper('formatDate', function(date) {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const day = String(d.getDate()).padStart(2, '0');
+    return `${year}/${day}/${month}`;
+  });
   // database configuration
   const dbConfig = {
     host: 'db', // the database server
