b830e057f356a1f7a86f8fbcdf657245219cfaff
Devin Green <degr3336@Colorado.edu>
2024-04-11 13:56:02 -0600

    create home page template

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -44,8 +44,34 @@ db.connect()
   .catch(error => {
     console.log('ERROR:', error.message || error);
   });
+  extname: 'hbs',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials',
+});
+
+// database configuration
+const dbConfig = {
+  host: 'db', // the database server
+  port: 5432, // the database port
+  database: process.env.POSTGRES_DB, // the database name
+  user: process.env.POSTGRES_USER, // the user account to connect with
+  password: process.env.POSTGRES_PASSWORD, // the password of the user account
+};
+
+const db = pgp(dbConfig);
+
+// test your database
+db.connect()
+  .then(obj => {
+    console.log('Database connection successful'); // you can view this message in the docker compose logs
+    obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+    console.log('ERROR:', error.message || error);
+  });
 
 
+// *****************************************************
 // *****************************************************
 // <!-- Section 3 : App Settings -->
 // *****************************************************
@@ -107,7 +133,28 @@ app.get('/register', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
+app.get('/login', (req, res) => {
   res.render('pages/login');
+});
+
+app.get('/home', (req, res) => {
+  if (req.session.user) {
+
+    const query = "SELECT trip_title, start_date , number_of_days, trip_progress FROM trips WHERE username = $1;";
+
+    db.any(query, [user.username])
+      .then(data => {
+        res.render('pages/home', { data: data });
+      })
+      .catch((err) => {
+        res.render('pages/home', { data: [] });
+        console.log("error")
+      });
+
+
+  } else {
+    res.redirect('/login'); // redirect users to login page  
+  }
 });
 
 // Register
@@ -115,54 +162,65 @@ app.post('/register', async (req, res) => {
   //hash the password using bcrypt library
   const hash = await bcrypt.hash(req.body.password, 10);
 
+
   // To-DO: Insert username and hashed password into the 'users' table
   const username = req.body.username;
   const email = req.body.email;
 
+  if (!username || !hash || !email) {
+    return res.status(400).send('Missing required fields');
+  }
+
   const query = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3);`;
   db.any(query, [username, hash, email])
     .then(data => {
-      res.redirect('/home')
+      res.status(200);
+      res.redirect('/login')
     })
     .catch((err) => {
+      res.status(400);
       res.redirect('/register')
       console.log("error")
     });
 });
 
 app.post('/login', (req, res) => {
+app.post('/login', (req, res) => {
   const username = req.body.username;
   // const password = req.body.password;
 
   const query = `SELECT username, password, email from users WHERE username = $1;`;
 
+
+  const query = `SELECT username, password, email from users WHERE username = $1;`;
+
   db.one(query, [username])
-  .then( async (data) =>{
-    const match = await bcrypt.compare(req.body.password, data.password);
-    
-    if(match){ // login successesful
-      user.username = data.username; // save data to the user object 
-      user.password = data.password;
-      user.email = data.email;
-      
-      req.session.user = user;
-      req.session.save();
-      res.status(200);
-      res.redirect("/home")
-    }
-    else{
+    .then(async (data) => {
+      const match = await bcrypt.compare(req.body.password, data.password);
+
+      if (match) { // login successesful
+        user.username = data.username; // save data to the user object 
+        user.password = data.password;
+        user.email = data.email;
+
+        req.session.user = user;
+        req.session.save();
+        res.status(200);
+        res.redirect("/home")
+      }
+      else {
+        res.status(400);
+        res.render('pages/login', {
+          error: true,
+          message: 'Incorrect username or password'
+        })
+      }
+    })
+    .catch(err => {
       res.status(400);
-      res.render('pages/login',{
-        error: true,
-        message: 'Incorrect username or password'
-      })
-    }
-  })
-  .catch(err =>{
-    res.status(400);
-  })
-  
-  
+    })
+
+
 });
 
 // Authentication Middleware.
@@ -186,8 +244,8 @@ app.get('/alltrips', (req, res) => {
 });
 
 app.get('/createTrip', (req, res) => {
-  res.render('pages/createTrip',{
-    
+  res.render('pages/createTrip', {
+
   });
 });
 
@@ -197,12 +255,12 @@ app.get('/home', (req, res) => {
 
 
 
-app.post("/createTrip",(req, res) =>{
+app.post("/createTrip", (req, res) => {
   const title = req.body.title;
   const startdate = req.body.startdate;
   const numDays = req.body.numdays;
   const username = user.username;
-  if(!username){
+  if (!username) {
     res.status(400).send("How did you even get this far without logging in???")
   }
   const trip_progress = "Planned"; // this is default
@@ -214,12 +272,12 @@ app.post("/createTrip",(req, res) =>{
     VALUES ($1, $2, $3, $4, $5)
     RETURNING trip_id, number_of_days; 
   `
-  
+
   const queryDays = `
     INSERT INTO days (day_number, trip_id) VALUES ($1, $2);
   `
-  
-  db.task(async task=>{
+
+  db.task(async task => {
     // result will have trip_id and number_of_days
     const result = await task.one(queryTrips, [title, startdate, numDays, username, trip_progress]);
     for(let i = 1; i <=  numDays; i++){
@@ -229,18 +287,18 @@ app.post("/createTrip",(req, res) =>{
     
     
   })
-  .then(data=>{
-    res.render('pages/home',{
-      message: "Created Trip Successfully!"
+    .then(data => {
+      res.render('pages/home', {
+        message: "Created Trip Successfully!"
+      })
     })
-  })
-  .catch(err=>{
-    res.render('pages/home',{
-      error: true,
-      message: "Could not create the trip!"
+    .catch(err => {
+      res.render('pages/home', {
+        error: true,
+        message: "Could not create the trip!"
+      })
+      console.log("ERROR create trips did not work")
     })
-    console.log("ERROR create trips did not work")
-  })
 
 });
 
