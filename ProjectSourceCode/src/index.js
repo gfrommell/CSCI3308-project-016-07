2cb01c387b146683f8ebebb1cb04d5e2cb5a51b0
Derick Sayavong <dsayavong54651@gmail.com>
2024-04-18 15:23:17 -0600

    added tehya's changes

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -85,7 +85,48 @@ Handlebars.registerHelper('eventForDay', function(events, index) {
           accum.push(events[i]);
         }
     }
-    console.log(accum);
+    return accum;
+  }
+});
+
+Handlebars.registerHelper('campsForDay', function(camps, index) {
+  var accum = [];
+  if(camps == undefined){
+    return;
+  } else {
+    for(var i = 0; i < camps.length; ++i) {
+        if(camps[i].day_number == index){
+          accum.push(camps[i]);
+        }
+    }
+    return accum;
+  }
+});
+
+Handlebars.registerHelper('thingsForDay', function(things, index) {
+  var accum = [];
+  if(things == undefined){
+    return;
+  } else {
+    for(var i = 0; i < things.length; ++i) {
+        if(things[i].day_number == index){
+          accum.push(things[i]);
+        }
+    }
+    return accum;
+  }
+});
+
+Handlebars.registerHelper('toursForDay', function(tours, index) {
+  var accum = [];
+  if(tours == undefined){
+    return;
+  } else {
+    for(var i = 0; i < tours.length; ++i) {
+        if(tours[i].day_number == index){
+          accum.push(tours[i]);
+        }
+    }
     return accum;
   }
 });
@@ -565,7 +606,7 @@ app.get('/edit/:id/:day_id?', (req, res) => {
     `;
 
     //return await task.batch([task.one(query, id), task.any(parks, id), task.any(events, id), task.any(tours, id), task.any(things, id), task.any(camps, id)]);
-    return await task.batch([task.one(query, id), task.any(parks, id), task.any(events, id)]);
+    return await task.batch([task.one(query, id), task.any(parks, id), task.any(events, id), task.any(tours, id), task.any(things, id), task.any(campgrounds, id)]);
   })
   .then(data=>{
     
@@ -579,9 +620,9 @@ app.get('/edit/:id/:day_id?', (req, res) => {
       trip: data[0],
       parks: data[1],
       events: data[2],
-     // tours: data[3],
-      //things: data[4],
-      //camps: data[5],
+      tours: data[3],
+      things: data[4],
+      camps: data[5],
       message: "Fetched data"
     })
   })
@@ -751,7 +792,7 @@ app.get('/:trip_id/edit/:day_id/:park_code', (req, res) =>{
   
 })
 
-app.post('/insert-event/:day_id/:event_id', (req, res) =>{
+app.post('/insert/:day_id/:event_id', (req, res) =>{
   // Add specific item to that day
   const day_id = req.params.day_id
   const event_id = req.params.event_id;
@@ -759,23 +800,6 @@ app.post('/insert-event/:day_id/:event_id', (req, res) =>{
   `INSERT INTO days_to_events (day_id, event_id) VALUES ($1, $2)`
 
   db.none(query, [day_id, event_id])
-  .then(()=>[
-    res.redirect('/home')
-  ])
-  .catch(err=>{
-    console.log("insert items error")
-  })
-  
-})
-
-app.post('/insert-campground/:day_id/:campground_id', (req, res) =>{
-  // Add specific item to that day
-  const day_id = req.params.day_id
-  const campground_id = req.params.campground_id;
-  const query =
-  `INSERT INTO days_to_campgrounds (campground_id, day_id) VALUES ($1, $2)`
-
-  db.none(query, [campground_id, day_id])
   .then(()=>[
     res.redirect('/home')
   ])
